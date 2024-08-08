a8a0e9c3d7b28960395d9008de7471e2ba3ed430
Mariele Ponticiello <mariele.ponticiello@temple.edu>
2024-08-08 11:36:52 -0400

    commit app

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Vue 3 + Vite
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+Simple Vue App that uses Leaflet maps to render map of user's location.
 
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+![alt text](./ss.png)
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {onMounted, ref} from 'vue';
+import L from 'leaflet';
+
+const lat = ref(0);
+const lng = ref(0);
+const map = ref();
+const mapContainer = ref();
+
+onMounted(() => {
+  map.value = L.map(mapContainer.value).setView([32.5317397, -117.0195290], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map.value);
+})
+
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.watchPosition((position) => {
+          lat.value = position.coords.latitude;
+          lng.value = position.coords.longitude;
+          map.value.setView([lat.value, lng.value], 13);
+
+          L.marker([lat.value, lng.value],{draggable : true})
+          .addTo(map.value)
+          .on("dragend",(event)=>{
+            console.log(event);
+          });
+      })
+    }
+}
+
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button @click="getLocation()">Get Location</button>
+  {{ lat }} , {{ lng }}
+
+  <div ref="mapContainer" style="width: 40rem;height:40rem"></div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
